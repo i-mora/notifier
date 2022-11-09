f92bb293c85c17583f85b332d1ba73d179a5f333
Alejandro Mora <arenteria-itexico@ordermygear.com>
2022-11-09 08:25:26 -0600

    Update creds.json

--- internal/google/config.go
+++ internal/google/config.go
@@ -3,9 +3,10 @@ package google
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"strings"
+	"os"
 
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
@@ -16,21 +17,23 @@ import (
 
 const (
 	viperGmailCredentialsJSON = "gmail.credentials.json"
-	viperGmailToken           = "gmail.token"
 )
 
 func init() {
 	viper.BindEnv(viperGmailCredentialsJSON, "GMAIL_CREDENTIALS_JSON")
-	viper.BindEnv(viperGmailToken, "GMAIL_TOKEN")
 }
 
 func NewService() *gmail.Service {
 	ctx := context.Background()
 
 	credentials := viper.GetString(viperGmailCredentialsJSON)
+	b, err := os.ReadFile(credentials)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
 
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON([]byte(credentials), gmail.GmailSendScope)
+	config, err := google.ConfigFromJSON(b, gmail.GmailSendScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
@@ -44,17 +47,57 @@ func NewService() *gmail.Service {
 	return srv
 }
 
-// getClient retrieve a token, saves the token, then returns the generated client.
+// Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
-	rawToken := viper.GetString(viperGmailToken)
-
-	tok := &oauth2.Token{}
-	err := json.NewDecoder(strings.NewReader(rawToken)).Decode(tok)
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
 	if err != nil {
-		log.Fatalf("Unable to decode Gmail token: %v", err)
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
 	}
 	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
