21795e9131231f4474f616e9937c0f6c895c70ef
Alejandro Mora <arenteria-itexico@ordermygear.com>
2022-11-09 00:30:49 -0600

    Add Gmail token control

--- internal/google/config.go
+++ internal/google/config.go
@@ -3,10 +3,9 @@ package google
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"strings"
 
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
@@ -17,10 +16,12 @@ import (
 
 const (
 	viperGmailCredentialsJSON = "gmail.credentials.json"
+	viperGmailToken           = "gmail.token"
 )
 
 func init() {
 	viper.BindEnv(viperGmailCredentialsJSON, "GMAIL_CREDENTIALS_JSON")
+	viper.BindEnv(viperGmailToken, "GMAIL_TOKEN")
 }
 
 func NewService() *gmail.Service {
@@ -48,52 +49,12 @@ func getClient(config *oauth2.Config) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
+	rawToken := viper.GetString(viperGmailToken)
+
+	tok := &oauth2.Token{}
+	err := json.NewDecoder(strings.NewReader(rawToken)).Decode(tok)
 	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
+		log.Fatalf("Unable to decode Gmail token: %v", err)
 	}
 	return config.Client(context.Background(), tok)
-}
-
-// getTokenFromWeb request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
-// tokenFromFile retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// saveToken saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
 }
